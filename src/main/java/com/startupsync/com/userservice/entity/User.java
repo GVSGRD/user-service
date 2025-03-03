1a0fc3263fa8920083a0a38bf4ad5606ce8238cc
ganeshvenkatasai <ganeshmanchi123@gmail.com>
2025-03-03 23:25:56 +0530

    Add Docker Repo

--- src/main/java/com/startupsync/com/userservice/entity/User.java
+++ src/main/java/com/startupsync/com/userservice/entity/User.java
@@ -58,5 +58,4 @@ public class User {
     @Temporal(TemporalType.TIMESTAMP)
     @Column(name = "updated_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
     private Date updatedAt;
-    
 }
